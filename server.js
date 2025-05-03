469a854926598170c304dead684ecdda9f2cb3ac
iamVedanta <callvedanta@gmail.com>
2025-05-03 09:41:06 +0530

    addeduse experss.json()

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ dotenv.config();
 
 MONGO_URI = process.env.MONGO_URI;
 const app = express();
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing form data
 
 app.use("/api/auth", authRoutes);
 app.use(express.json());
